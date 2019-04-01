dded4b0dd77dcabd3323addcb9ca7af4fc76b5b5
tomoshuw <44280020+tomoshuw@users.noreply.github.com>
2019-04-01 13:51:00 +0100

    Add files via upload

---
+++ recommend.py
@@ -0,0 +1,14 @@
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import operator
+
+raw_history = open('history.txt', 'r')
+
+history = raw_history.readlines()
+data = history[0].split()
+
+transactions = data[2]
+customers = data[0]
+items = data[1]
